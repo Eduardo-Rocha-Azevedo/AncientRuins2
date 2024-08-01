9075cfa344052cfba696f312bf16559d0bb15d71
Eduardo Rocha Azevedo <142415927+Eduardo-Rocha-Azevedo@users.noreply.github.com>
2024-08-01 19:47:07 -0300

    desenvolvimento basico

---
+++ src/principal/Main.java
@@ -0,0 +1,23 @@
+package principal;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Ancient Ruins 2");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.setupGame();
+        gamePanel.startGameThread();
+    }
+}
